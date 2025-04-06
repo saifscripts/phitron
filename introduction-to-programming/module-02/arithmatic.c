1eca326b33eda9f68fac7cde43277d0a04b617f0
Md Saif Ullah <mdsaifullah.wd@gmail.com>
2025-04-06 20:22:46 +0600

    arithmatic operators

---
+++ introduction-to-programming/module-02/arithmatic.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main ()
+{
+    int num1 = 15;
+    float num2 = 2;
+    // scanf("%d %d", num1, num2);
+    float div = num1 / num2;
+    printf("Division: %f", div);
+
+    return 0;
+}
